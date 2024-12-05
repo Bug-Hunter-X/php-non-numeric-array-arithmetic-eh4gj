41276a0ce756c1aebd27d62deb3f6afa361b2189
BugHunter-X <askzenai@gmail.com>
2024-12-05 09:00:06 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,15 @@
+```php
+function calculateSum($numbers) {
+  $sum = 0;
+  foreach ($numbers as $number) {
+    $sum += $number;
+  }
+  return $sum;
+}
+
+$numbers = [1, 2, 3, 4, 5];
+echo calculateSum($numbers); // Output: 15
+
+$numbers = [1, 2, 'a', 4, 5];
+echo calculateSum($numbers); // Warning: A non-numeric value encountered
+```
